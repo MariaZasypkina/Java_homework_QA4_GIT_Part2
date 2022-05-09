cf8d4cb5c4c94938bd70d23eaac3b4531c09ea9c
Maria Zasypkina <maria.zasypkina@gmail.com>
2022-05-09 12:09:16 -0600

    HW7

--- src/HW7/EmployeeUtils.java
+++ src/HW7/EmployeeUtils.java
@@ -1,14 +1,16 @@
 package HW7;
+
 public class EmployeeUtils {
-public static Worker findWorkerByName(Worker[] workers, String name){
-for (int i = 0; i < workers.length; i++) {
-  if (name.equals(workers[i].getName())) {
-      return workers[i];
-  }
+    public static Worker findWorkerByName(Worker[] workers, String name) {
+        for (int i = 0; i < workers.length; i++) {
+            if (name.equals(workers[i].getName())) {
+                return workers[i];
+            }
+        }
+        return null;
     }
-return null;
-}
-    public static Worker findWorkerByNameSubName(Worker[] workers, String subName){
+
+    public static Worker findWorkerByNameSubName(Worker[] workers, String subName) {
         for (int i = 0; i < workers.length; i++) {
             if (workers[i].getName().contains(subName)) {
                 return workers[i];
@@ -16,34 +18,37 @@ return null;
         }
         return null;
     }
-    public static int salarySun(Worker[] workers){
-    int sum = 0;
+
+    public static int salarySun(Worker[] workers) {
+        int sum = 0;
         for (int i = 0; i < workers.length; i++) {
-           sum += workers[i].getSalary();
+            sum += workers[i].getSalary();
         }
         return sum;
     }
-public static int maxSalary(Worker[] workers){
-    int maxSal = 0;
-    for (int i = 0; i < workers.length; i++){
-        if (workers[i].getSalary() > maxSal) {
-            maxSal = workers[i].getSalary();
+
+    public static int maxSalary(Worker[] workers) {
+        int maxSal = 0;
+        for (int i = 0; i < workers.length; i++) {
+            if (workers[i].getSalary() > maxSal) {
+                maxSal = workers[i].getSalary();
+            }
         }
-    }
-    return maxSal;
+        return maxSal;
 
 
-}
-public static int minSalary(Worker[] workers){
-    int minSal = 0;
-    for (int i = 1; i < workers.length; i++){
-        minSal = workers[0].getSalary();
-        if (workers[i].getSalary() < minSal){
-            minSal = workers[i].getSalary();
-        }
     }
-    return minSal;
-}
+
+    public static int minSalary(Worker[] workers) {
+        int minSal = 0;
+        for (int i = 1; i < workers.length; i++) {
+            minSal = workers[0].getSalary();
+            if (workers[i].getSalary() < minSal) {
+                minSal = workers[i].getSalary();
+            }
+        }
+        return minSal;
+    }
 
 }
 
